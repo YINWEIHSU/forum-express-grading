30f480544f56a708f4d31869df07e9a9e1cedca9
YinWei <cba5530@gmail.com>
2021-02-03 23:49:11 +0800

    feat: Add restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
+const restController = require('../controllers/restControllers')
+const adminController = require('../controllers/adminController')
+
 module.exports = (app) => {
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  //如果使用者訪問首頁，就導向 /restaurants 的頁面
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
+  app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
+
 }

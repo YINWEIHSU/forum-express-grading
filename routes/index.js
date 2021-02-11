149ec9560175821e5519429733035b64f20e335d
unknown <cba5530@gmail.com>
2021-02-11 16:09:49 +0800

    Create category model & Add restaurant.CategoryId and association

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const helper = require('../_helpers')
+const helpers = require('../_helpers')
 
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js') 
@@ -34,6 +34,8 @@ module.exports = (app, passport) => {
  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
  app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+ app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
+ app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
 
  app.get('/signup', userController.signUpPage)
  app.post('/signup', userController.signUp)

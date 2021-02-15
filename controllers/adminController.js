b271db90f491efae8c15049e115980bc49f4ba52
張濰 <choui@zhangweideMacBook-Pro.local>
2021-02-15 11:54:48 +0800

    Show category name on admin/restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -57,7 +57,7 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+    return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: [Category] }).then(restaurant => {
       return res.render('admin/restaurant', {
         restaurant: restaurant
       })

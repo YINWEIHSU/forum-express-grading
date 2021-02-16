4872ff4547e57c460ea513f6318a6d746250467c
YinWei <cba5530@gmail.com>
2021-02-16 16:57:33 +0800

    Add user put feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const userController = {
   signUpPage: (req, res) => {
@@ -55,8 +57,46 @@ const userController = {
   },
   editUser: (req, res) => {
     User.findByPk(req.params.id).then(user => {
-      return res.render('edit', { user })
+      return res.render('edit', { user: user.toJSON() })
     })
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            console.log(user)
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : restaurant.image
+            })
+              .then((user) => {
+                req.flash('success_messages', 'Profile was successfully to update')
+                res.redirect(`/users/${user.id}`)
+              })
+          })
+      })
+    }
+    else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          restaurant.update({
+            name: req.body.name,
+            image: restaurant.image
+          })
+            .then((user) => {
+              req.flash('success_messages', 'Profile was successfully to update')
+              res.redirect(`/users/${user.id}`)
+            })
+        })
+    }
   },
 }
 

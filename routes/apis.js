080d471c54a6a824cca24ad5008729424d7d1243
YinWei <cba5530@gmail.com>
2021-02-24 23:58:07 +0800

    PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -13,5 +13,6 @@ router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 module.exports = router
